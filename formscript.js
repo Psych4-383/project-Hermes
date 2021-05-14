fe65eaff6224dbdd220a6d43d0b0b0cb210ed5f3
Psych4-383 <pikachu.bisht.67@gmail.com>
2021-05-14 13:42:47 +0530

    Database Connected and Authentication code corrected

--- formscript.js
+++ formscript.js
@@ -12,3 +12,19 @@ function submit(){
     location.href = '/index.html'
     return false
 }
+
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://accounts-7710.restdb.io/rest/new-collection",
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "609be450e3b6e02545eda819",
+      "cache-control": "no-cache"
+    }
+  }
+  
+$.ajax(settings).done(function (response) {
+    console.log(response);
+});
--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 window.onload = () => {
-    localStorage.setItem('loggedIn', false)
-}
+    localStorage.setItem('loggedIn', false);
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = false;
 
-function authenticate(){
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            window.accountsArray = JSON.parse(xhr.response)
+        } else {
+            console.log('Error')
+        }
+    }
+
+    xhr.open("GET", "https://accounts-7710.restdb.io/rest/new-collection");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.setRequestHeader("x-apikey", "609be450e3b6e02545eda819");
+
+    xhr.send();
+}
+function authenticate() {
     const popUp = document.getElementById('popUp');
     popUp.style.display = 'flex';
 }
-function goBack(){
+function goBack() {
     const popUp = document.getElementById('popUp');
     popUp.style.display = 'none';
 }
-function validate(){
+function validate() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('pswd').value;
-    if (username == '1' && password == '2'){
-        location.href = '/Form.html'
-        localStorage.setItem('username', username)
-        localStorage.setItem('loggenIn', true)
-        return false;
-    } else {
-        alert('Incorrect Username or Password')
-        location.href = '/index.html'
+    for (let item = 0; item < accountsArray.length; item++) {
+        if (username == accountsArray[item].username && password == accountsArray[item].password) {
+            location.href = '/Form.html'
+            localStorage.setItem('username', username)
+            localStorage.setItem('loggenIn', true)
+            console.log('Logged In!')
+            return false;
+        } else {
+            continue;
+        }
     }
+    alert('Incorrect Username or password');
+    location.reload()
 }
+
